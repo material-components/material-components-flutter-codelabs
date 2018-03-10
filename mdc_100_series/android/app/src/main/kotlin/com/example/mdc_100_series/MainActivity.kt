279d9708408a0baf43eab650d0a1f6b85e9a458d
Will Larche <larche@google.com>
2018-03-10 17:47:22 -0500

    [101] Starter code.
    Co-authored-by: Tianguang <penn.zht@gmail.com>
    Co-authored-by: Pierre-Louis <plg@google.com>
    Co-authored-by: Rody Davis <rody.davis.jr@gmail.com>

---
+++ mdc_100_series/android/app/src/main/kotlin/com/example/mdc_100_series/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mdc_100_series
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ mdc_100_series/android/app/src/main/kotlin/com/example/shrine/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.shrine
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
