a1ab2c728b7f1c082a02ae6d9b0c9d25bc14c46c
Will Larche <larche@google.com>
2018-03-30 21:48:53 -0400

    [101] Complete.

---
+++ MDC-101/complete/lib/app.dart
@@ -0,0 +1,19 @@
+import 'home.dart';
+import 'login.dart';
+
+import 'package:flutter/material.dart';
+
+class ShrineApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shrine',
+      home: HomePage(),
+      initialRoute: '/home/login',
+      routes: <String, WidgetBuilder>{
+        '/home': (BuildContext context) => new HomePage(),
+        '/home/login': (BuildContext context) => new LoginPage()
+      },
+    );
+  }
+}
---
+++ MDC-101/complete/lib/home.dart
@@ -0,0 +1,22 @@
+import 'login.dart';
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('SHRINE'),
+      ),
+      body: Center(
+        child: Text('You did it!'),
+      ),
+    );
+  }
+}
