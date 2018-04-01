8dd59afe4b8c7a406f8323557a293a56b26fdfcb
Will Larche <larche@google.com>
2018-03-31 22:12:51 -0400

    [102] Almost complete layout.

--- MDC-102/complete/lib/home.dart
+++ MDC-102/complete/lib/home.dart
@@ -1,5 +1,46 @@
 import 'package:flutter/material.dart';
 
+List<Card> _buildGridCards(BuildContext context, int count) {
+  List<Card> cards = List.generate(
+      count,
+      (int index) => Card(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                AspectRatio(
+                  aspectRatio: 16 / 11,
+                  child: Image.asset(
+                    'assets/$index-1.jpg',
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+                Expanded(
+                    child: Padding(
+                  padding: EdgeInsets.fromLTRB(
+                    16.0,
+                    12.0,
+                    16.0,
+                    8.0,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      // TODO(larche): Make headline6 when available
+                      Text('Tea Cups',
+                          style: Theme.of(context).textTheme.caption),
+                      //SizedBox(height: 8.0,),
+                      // TODO(larche): Make subtitle2 when available
+                      Text('\$36', style: Theme.of(context).textTheme.body2),
+                    ],
+                  ),
+                ))
+              ],
+            ),
+          ));
+
+  return cards;
+}
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -30,7 +71,12 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
       body: Center(
-        child: Text('You did it!'),
+        child: GridView.count(
+          crossAxisCount: 2,
+          children: _buildGridCards(context, 4),
+          padding: EdgeInsets.all(16.0),
+          mainAxisSpacing: 8.0,
+        ),
       ),
     );
   }
