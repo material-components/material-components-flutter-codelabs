12f0fcdf63cb5c356e159847a73c4abac0c025b8
Will Larche <larche@google.com>
2018-04-02 14:39:35 -0600

    [102] Dummy data.

---
+++ MDC-102/complete/lib/model/data.dart
@@ -0,0 +1,8 @@
+import 'product.dart';
+
+const List<Product> allProducts = <Product> [
+  Product(Category.home, 0, true, 'Tea Set', 35.0),
+  Product(Category.accessories, 1, false, 'Chintz Tie', 80.2),
+  Product(Category.clothing, 2, false, 'French Pants', 100.0),
+  Product(Category.home, 3, false, 'Stuffed Cat', 5.0),
+];
--- MDC-102/complete/lib/model/product.dart
+++ MDC-102/complete/lib/model/product.dart
@@ -1,14 +1,25 @@
+enum Category {
+  none,
+  accessories,
+  clothing,
+  home
+}
+
 class Product {
+  final Category category;
+
   /// The unique identifier of the product.
   final int id;
-
-  /// The default text to display.
-  final String name;
 
   /// A color associated with the product.
   final bool isFeatured;
 
-  const Product(this.id, this.name, this.color);
+  /// The default text to display.
+  final String name;
+
+  final double price;
+
+  const Product(this.category, this.id, this.isFeatured, this.name, this.price);
 
   @override
   String toString() => "$name (id=$id)";
