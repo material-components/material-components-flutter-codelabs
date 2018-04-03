af4cab3feb82c0f8f4f7bca9591f9e444fd7e094
Will Larche <larche@google.com>
2018-04-02 19:03:12 -0600

    [102] Fake data.

--- MDC-102/complete/lib/home.dart
+++ MDC-102/complete/lib/home.dart
@@ -4,6 +4,45 @@ import 'package:intl/intl.dart';
 import 'model/data.dart';
 import 'model/product.dart';
 
+List<Card> _buildGridCards(BuildContext context, List<Product> products) {
+  List<Card> cards = List.generate(
+      products.length,
+      (int index) => Card(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                AspectRatio(
+                  aspectRatio: 16 / 11,
+                  child: Image.asset(
+                    'assets/${products[index].id}-1.jpg',
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+                Expanded(
+                    child: Padding(
+                  padding: EdgeInsets.fromLTRB(
+                    16.0,
+                    12.0,
+                    16.0,
+                    8.0,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      // TODO(larche): Make headline6 when available
+                      Text(products[index].name,
+                          style: Theme.of(context).textTheme.caption),
+                      //SizedBox(height: 8.0,),
+                      // TODO(larche): Make subtitle2 when available
+                      Text('\$${products[index].price}',
+                          style: Theme.of(context).textTheme.body2),
+                    ],
+                  ),
+                ))
+              ],
+            ),
+          ));
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
