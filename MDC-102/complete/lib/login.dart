23adcf43daf24955a258c93801c336e37c812246
Will Larche <larche@google.com>
2018-04-04 16:43:14 -0700

    [102] Formatting

--- MDC-102/complete/lib/login.dart
+++ MDC-102/complete/lib/login.dart
@@ -4,59 +4,55 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: ListView(
-          padding: EdgeInsets.symmetric(horizontal: 24.0),
-          children: <Widget>[
-            SizedBox(height: 80.0),
-            Column(
-              children: <Widget>[
-                Image.asset('assets/diamond.png'),
-                SizedBox(
-                  height: 16.0,
-                ),
-                Text(
-                  'SHRINE',
-                ),
-              ],
-            ),
-            SizedBox(height: 120.0),
-            TextField(
-              decoration: InputDecoration(
-                filled: true,
-                labelText: 'Username',
+        body: SafeArea(
+          child: ListView(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            children: <Widget>[
+              SizedBox(height: 80.0),
+              Column(
+                children: <Widget>[
+                  Image.asset('assets/diamond.png'),
+                  SizedBox(height: 16.0),
+                  Text(
+                    'SHRINE',
+                  ),
+                ],
               ),
-            ),
-            SizedBox(height: 12.0),
-            TextField(
-              decoration: InputDecoration(
-                filled: true,
-                labelText: 'Password',
+              SizedBox(height: 120.0),
+              TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  labelText: 'Username',
+                ),
               ),
-              obscureText: true,
-            ),
-            SizedBox(height: 32.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                FlatButton(
-                  child: Text('CANCEL'),
-                  onPressed: null,
+              SizedBox(height: 12.0),
+              TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  labelText: 'Password',
                 ),
-                SizedBox(
-                  width: 16.0,
-                ),
-                RaisedButton(
-                  child: Text('NEXT'),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                )
-              ],
-            )
-          ],
-        ),
-      )
+                obscureText: true,
+              ),
+              SizedBox(height: 32.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  FlatButton(
+                    child: Text('CANCEL'),
+                    onPressed: null,
+                  ),
+                  SizedBox(width: 16.0),
+                  RaisedButton(
+                    child: Text('NEXT'),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  )
+                ],
+              )
+            ],
+          ),
+        )
     );
   }
 }
