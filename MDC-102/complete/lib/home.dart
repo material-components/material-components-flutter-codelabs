08ae71baca0b32268f3435af31b5497d7e8a6f1e
Will Larche <larche@google.com>
2018-04-04 16:43:05 -0700

    [102] Home layout

--- MDC-102/complete/lib/home.dart
+++ MDC-102/complete/lib/home.dart
@@ -7,41 +7,40 @@ List<Card> _buildGridCards(BuildContext context, List<Product> products) {
   List<Card> cards = List.generate(
       products.length,
       (int index) => Card(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            AspectRatio(
-              aspectRatio: 16 / 11,
-              child: Image.asset(
-                'assets/${products[index].id}-1.jpg',
-                fit: BoxFit.fitWidth,
-              ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                AspectRatio(
+                  aspectRatio: 16 / 11,
+                  child: Image.asset(
+                    'assets/${products[index].id}-1.jpg',
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+                Expanded(
+                    child: Padding(
+                  padding: EdgeInsets.fromLTRB(
+                    16.0,
+                    12.0,
+                    16.0,
+                    8.0,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      // TODO(larche): Make headline6 when available
+                      Text(products[index].name,
+                          style: Theme.of(context).textTheme.title, maxLines: 1,),
+                      SizedBox(height: 8.0,),
+                      // TODO(larche): Make subtitle2 when available
+                      Text('${products[index].priceString}',
+                          style: Theme.of(context).textTheme.body2),
+                    ],
+                  ),
+                ))
+              ],
             ),
-            Expanded(
-                child: Padding(
-              padding: EdgeInsets.fromLTRB(
-                16.0,
-                12.0,
-                16.0,
-                8.0,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  // TODO(larche): Make headline6 when available
-                  Text(products[index].name,
-                      style: Theme.of(context).textTheme.caption),
-                  //SizedBox(height: 8.0,),
-                  // TODO(larche): Make subtitle2 when available
-                  Text('${products[index].priceString}',
-                      style: Theme.of(context).textTheme.body2),
-                ],
-              ),
-            ))
-          ],
-        ),
-      )
-  );
+          ));
 
   return cards;
 }
@@ -52,12 +51,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  //getData()
-
   @override
   void initState() {
     super.initState();
-    getData().then((value) => setState((){}));
+    getData().then((value) => setState(() {}));
   }
 
   @override
@@ -86,9 +83,10 @@ class _HomePageState extends State<HomePage> {
       body: Center(
         child: GridView.count(
           crossAxisCount: 2,
-          children: _buildGridCards(context, allProducts),
+          children: _buildGridCards(context, generateProducts()),
           padding: EdgeInsets.all(16.0),
           mainAxisSpacing: 8.0,
+          childAspectRatio: 8.0 / 9.0,
         ),
       ),
     );
