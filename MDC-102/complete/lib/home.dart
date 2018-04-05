fff531cb1bd0b9a46c8a9fd1e7990fbf794ea08f
Will Larche <larche@google.com>
2018-04-05 16:11:34 -0700

    [102] PR feedback.

--- MDC-102/complete/lib/home.dart
+++ MDC-102/complete/lib/home.dart
@@ -7,44 +7,38 @@ List<Card> _buildGridCards(BuildContext context, List<Product> products) {
   List<Card> cards = List.generate(
       products.length,
       (int index) => Card(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              AspectRatio(
-                aspectRatio: 16 / 11,
-                child: Image.asset(
-                  'assets/${products[index].id}-1.jpg',
-                  fit: BoxFit.fitWidth,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                AspectRatio(
+                  aspectRatio: 16 / 11,
+                  child: Image.asset(
+                    'assets/${products[index].id}-1.jpg',
+                    fit: BoxFit.fitWidth,
+                  ),
                 ),
-              ),
-              Expanded(
-                  child: Padding(
-                padding: EdgeInsets.fromLTRB(
-                  16.0,
-                  12.0,
-                  16.0,
-                  8.0,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    // TODO(larche): Make headline6 when available
-                    Text(
-                      products[index].name,
-                      style: Theme.of(context).textTheme.title,
-                      maxLines: 1,
-                    ),
-                    SizedBox(height: 8.0),
-                    // TODO(larche): Make subtitle2 when available
-                    Text('${products[index].priceString}',
-                        style: Theme.of(context).textTheme.body2),
-                  ],
-                ),
-              ))
-            ],
-          ),
-      )
-  );
+                Expanded(
+                    child: Padding(
+                  padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      // TODO(larche): Make headline6 when available
+                      Text(
+                        products[index].name,
+                        style: Theme.of(context).textTheme.title,
+                        maxLines: 1,
+                      ),
+                      SizedBox(height: 8.0),
+                      // TODO(larche): Make subtitle2 when available
+                      Text(products[index].priceString,
+                          style: Theme.of(context).textTheme.body2),
+                    ],
+                  ),
+                ))
+              ],
+            ),
+          ));
 
   return cards;
 }
@@ -86,12 +80,11 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Center(
         child: GridView.count(
-          crossAxisCount: 2,
-          children: _buildGridCards(context, generateProducts()),
-          padding: EdgeInsets.all(16.0),
-          mainAxisSpacing: 8.0,
-          childAspectRatio: 8.0 / 9.0,
-        ),
+            crossAxisCount: 2,
+            children: _buildGridCards(context, generateProducts()),
+            padding: EdgeInsets.all(16.0),
+            mainAxisSpacing: 8.0,
+            childAspectRatio: 8.0 / 9.0),
       ),
     );
   }
--- MDC-102/complete/lib/model/data.dart
+++ MDC-102/complete/lib/model/data.dart
@@ -15,37 +15,11 @@ Future<Null> getData() async {
 
 List<Product> generateProducts() {
   return <Product>[
-    Product(
-      Category.home,
-      0,
-      true,
-      'Tab Can',
-      35.0,
-      formatter.format(35.0),
-    ),
-    Product(
-      Category.accessories,
-      1,
-      false,
-      'Pineapple Wallpaper',
-      80.2,
-      formatter.format(80.2),
-    ),
-    Product(
-      Category.clothing,
-      2,
-      false,
-      'Tab & Fresca Cooler',
-      100.0,
-      formatter.format(100.0),
-    ),
-    Product(
-      Category.home,
-      3,
-      false,
-      'Capris',
-      5.0,
-      formatter.format(5.0),
-    ),
+    Product(Category.home, 0, true, 'Tab Can', 35.0, formatter.format(35.0)),
+    Product(Category.accessories, 1, false, 'Pineapple Wallpaper', 80.2,
+        formatter.format(80.2)),
+    Product(Category.clothing, 2, false, 'Tab & Fresca Cooler', 100.0,
+        formatter.format(100.0)),
+    Product(Category.home, 3, false, 'Capris', 5.0, formatter.format(5.0)),
   ];
 }
