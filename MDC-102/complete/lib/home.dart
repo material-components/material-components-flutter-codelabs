804726197e0bd4b6fd352266bb41033f4b8b91da
Will Larche <larche@google.com>
2018-04-09 17:09:54 -0700

    [102] PR feedback.

--- MDC-102/complete/lib/home.dart
+++ MDC-102/complete/lib/home.dart
@@ -10,45 +10,46 @@ List<Card> _buildGridCards(BuildContext context, List<Product> products) {
   }
 
   final ThemeData theme = Theme.of(context);
-  final NumberFormat formatter = NumberFormat.simpleCurrency(locale: Localizations.localeOf(context).toString());
+  final NumberFormat formatter = NumberFormat.simpleCurrency(
+      locale: Localizations.localeOf(context).toString());
   return List.generate(products.length, (int index) {
     return Card(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              AspectRatio(
-                aspectRatio: 16 / 11,
-                child: Image.asset(
-                  'assets/${products[index].id}-1.jpg',
-                  fit: BoxFit.fitWidth,
-                ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      // TODO(larche): Make headline6 when available
-                      Text(
-                        products[index].name,
-                        style: theme.textTheme.title,
-                        maxLines: 1,
-                      ),
-                      SizedBox(height: 8.0),
-                      // TODO(larche): Make subtitle2 when available
-                      Text(
-                        formatter.format(products[index].price),
-                        style: theme.textTheme.body2,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          AspectRatio(
+            aspectRatio: 16 / 11,
+            child: Image.asset(
+              'assets/${products[index].id}-1.jpg',
+              fit: BoxFit.fitWidth,
+            ),
           ),
-        );}
-  ).toList();
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  // TODO(larche): Make headline6 when available
+                  Text(
+                    products[index].name,
+                    style: theme.textTheme.title,
+                    maxLines: 1,
+                  ),
+                  SizedBox(height: 8.0),
+                  // TODO(larche): Make subtitle2 when available
+                  Text(
+                    formatter.format(products[index].price),
+                    style: theme.textTheme.body2,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  });
 }
 
 class HomePage extends StatefulWidget {
