574253b52613596d4b6b44390c0dbee325114b19
Will Larche <larche@google.com>
2018-04-25 19:49:51 -0400

    [103] Completed code.
    [101] Starter code.
    
    [101] Making AppBar something you add in 102.
    
    [Model] Adding model from the start.
    
    [103] Adding fonts.
    
    [103] Supplemental files.
    
    [101] Correcting supplemental files.
    
    [101] Copy correction.
    
    [101] Minor renaming.
    
    [101] Update for Dart 2.
    
    [101] Missing dependency.
    
    [101] Dart 2.
    
    [101] Dart 2.
    
    [101] Dart 2.
    
    [101] Dart 2.
    
    [101] Correcting model.
    
    [Meta] README file.
    
    [Meta] Clarification.
    
    [101] Copy correction.
    
    [104] Updating data.
    
    [101] Correcting widget class. (#23)
    
    Add link to actual codelab in the README
    
    [Meta] README correction.
    
    [101] README specificity.
    
    [101] Complete code.
    [102] Starter code.
    
    [102] Minor copy correction.
    
    [101] Update for Dart 2
    
    [102] Rebase.
    
    Add the slanted_menu.png to assets for 101 branch so its ready when needed. (#44) (#45)
    
    [102] Complete code.
    [103] Starter code.
    
    [102] Correcting home being stateful.
    
    [103] Correcting order of fields. (#10)
    
    [102] Removing unneeded value.
    
    [102] Dart 2.
    
    [102] Data correction.
    
    [103] README specificity.
    
    [103] Completed code.
    [104] Starter code.
    
    [103] Removing unneeded directory.
    
    [103] Unused icons code.
    
    [103] Moving supplemental files.
    
    [103] Using recent changes.
    
    [103] Removing unused import.
    
    [103] Data correction.
    
    [103] Adding body2 styling.
    
    [103] Dart 2
    
    [103] Color const correction.
    
    [104] README specificity.

--- mdc_100_series/lib/app.dart
+++ mdc_100_series/lib/app.dart
@@ -14,8 +14,10 @@
 
 import 'package:flutter/material.dart';
 
+import 'colors.dart';
 import 'home.dart';
 import 'login.dart';
+import 'supplemental/cut_corners_border.dart';
 
 class ShrineApp extends StatelessWidget {
   @override
@@ -25,18 +27,69 @@ class ShrineApp extends StatelessWidget {
       home: HomePage(),
       initialRoute: '/login',
       onGenerateRoute: _getRoute,
+      theme: _kShrineTheme,
     );
   }
 
   Route<dynamic> _getRoute(RouteSettings settings) {
-    if (settings.name != '/login') {
-      return null;
+    if (settings.name == '/login') {
+      return MaterialPageRoute<void>(
+        settings: settings,
+        builder: (BuildContext context) => LoginPage(),
+        fullscreenDialog: true,
+      );
     }
 
-    return MaterialPageRoute<void>(
-      settings: settings,
-      builder: (BuildContext context) => LoginPage(),
-      fullscreenDialog: true,
-    );
+    return null;
   }
+}
+
+final ThemeData _kShrineTheme = _buildShrineTheme();
+
+ThemeData _buildShrineTheme() {
+  final ThemeData base = ThemeData.light();
+  return base.copyWith(
+    accentColor: kShrineBrown900,
+    primaryColor: kShrinePink100,
+    buttonColor: kShrinePink100,
+    scaffoldBackgroundColor: kShrineBackgroundWhite,
+    cardColor: kShrineBackgroundWhite,
+    textSelectionColor: kShrinePink100,
+    errorColor: kShrineErrorRed,
+    buttonTheme: ButtonThemeData(
+      textTheme: ButtonTextTheme.accent,
+    ),
+    primaryIconTheme: base.iconTheme.copyWith(
+        color: kShrineBrown900
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      border: CutCornersBorder(),
+    ),
+    textTheme: _buildShrineTextTheme(base.textTheme),
+    primaryTextTheme: _buildShrineTextTheme(base.primaryTextTheme),
+    accentTextTheme: _buildShrineTextTheme(base.accentTextTheme),
+  );
+}
+
+TextTheme _buildShrineTextTheme(TextTheme base) {
+  return base.copyWith(
+    headline: base.headline.copyWith(
+      fontWeight: FontWeight.w500,
+    ),
+    title: base.title.copyWith(
+        fontSize: 18.0
+    ),
+    caption: base.caption.copyWith(
+      fontWeight: FontWeight.w400,
+      fontSize: 14.0,
+    ),
+    body2: base.body2.copyWith(
+      fontWeight: FontWeight.w500,
+      fontSize: 16.0,
+    ),
+  ).apply(
+    fontFamily: 'Rubik',
+    displayColor: kShrineBrown900,
+    bodyColor: kShrineBrown900,
+  );
 }
