25ac69c91cb2fd50bf0b715be52966130bb03091
Will Larche <larche@google.com>
2018-04-25 19:59:12 -0400

    [102] Complete code. [103] Starter code.

--- mdc_100_series/lib/home.dart
+++ mdc_100_series/lib/home.dart
@@ -13,13 +13,103 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class HomePage extends StatelessWidget {
+import 'model/data.dart';
+import 'model/product.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Card> _buildGridCards() {
+    List<Product> products = getAllProducts();
+
+    if (products == null || products.isEmpty) {
+      return const <Card>[];
+    }
+
+    final ThemeData theme = Theme.of(context);
+    final NumberFormat formatter = new NumberFormat.simpleCurrency(
+        locale: Localizations.localeOf(context).toString());
+
+    return products.map((product) {
+      return new Card(
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new AspectRatio(
+              aspectRatio: 18 / 11,
+              child: new Image.asset(
+                product.assetName,
+                package: product.assetPackage,
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+            new Expanded(
+              child: new Padding(
+                padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                child: new Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    // TODO(larche): Make headline6 when available
+                    new Text(
+                      product.name,
+                      style: theme.textTheme.title,
+                      maxLines: 1,
+                    ),
+                    const SizedBox(height: 8.0),
+                    // TODO(larche): Make subtitle2 when available
+                    new Text(
+                      formatter.format(product.price),
+                      style: theme.textTheme.body2,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }).toList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: const Center(
-        child: const Text('You did it!'),
+      appBar: new AppBar(
+        leading: new IconButton(
+          icon: const Icon(Icons.menu),
+          onPressed: () {
+            print('Menu button');
+          },
+        ),
+        title: const Text('SHRINE'),
+        actions: <Widget>[
+          new IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {
+              print('Search button');
+            },
+          ),
+          new IconButton(
+            icon: const Icon(Icons.tune),
+            onPressed: () {
+              print('Filter button');
+            },
+          ),
+        ],
+      ),
+      body: new Center(
+        child: new GridView.count(
+          crossAxisCount: 2,
+          children: _buildGridCards(),
+          padding: const EdgeInsets.all(16.0),
+          mainAxisSpacing: 8.0,
+          childAspectRatio: 8.0 / 9.0,
+        ),
       ),
     );
   }
