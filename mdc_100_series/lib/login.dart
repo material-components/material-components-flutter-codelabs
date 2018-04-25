441c235746a4cc21531be899e31cf7921ed3dc08
Will Larche <larche@google.com>
2018-04-25 19:49:51 -0400

    [102] Completed code.
    [101] Starter code.
    
    [103] Supplemental files.
    
    [101] Correcting supplemental files.
    
    [101] Minor renaming.
    
    [101] Missing dependency.
    
    [101] Dart 2.
    
    [101] Dart 2.
    
    [101] Complete code.
    [102] Starter code.
    
    [102] Minor copy correction.
    
    [101] Update for Dart 2
    
    [102] Rebase.
    
    [102] starter todos (#54)

--- mdc_100_series/lib/login.dart
+++ mdc_100_series/lib/login.dart
@@ -16,29 +16,62 @@ import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
   @override
-  _LoginPageState createState() => new _LoginPageState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-  // TODO: Add text editing controllers (101)
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      body: new SafeArea(
-        child: new ListView(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
           children: <Widget>[
-            const SizedBox(height: 80.0),
-            new Column(
+            SizedBox(height: 80.0),
+            Column(
               children: <Widget>[
-                new Image.asset('assets/diamond.png'),
-                const SizedBox(height: 16.0),
-                const Text('SHRINE'),
+                Image.asset('assets/diamond.png'),
+                SizedBox(height: 16.0),
+                Text('SHRINE'),
               ],
             ),
-            const SizedBox(height: 120.0),
-            // TODO: Add TextField widgets (101)
-            // TODO: Add button bar (101)
+            SizedBox(height: 120.0),
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                filled: true,
+                labelText: 'Username',
+              ),
+            ),
+            SizedBox(height: 12.0),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                filled: true,
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
+            ButtonBar(
+              children: <Widget>[
+                FlatButton(
+                  child: Text('CANCEL'),
+                  onPressed: () {
+                    _usernameController.clear();
+                    _passwordController.clear();
+                  },
+                ),
+                RaisedButton(
+                  child: Text('NEXT'),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
           ],
         ),
       ),
