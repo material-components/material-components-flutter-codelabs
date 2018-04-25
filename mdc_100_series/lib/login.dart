8edc25e71c9ef971c27f1fb4c4835593062c78c8
Will Larche <larche@google.com>
2018-04-25 19:49:51 -0400

    [101] Starter code.
    [101] Making AppBar something you add in 102.
    
    [Model] Adding model from the start.
    
    [103] Adding fonts.
    
    [103] Supplemental files.
    
    [101] Correcting supplemental files.
    
    [101] Copy correction.
    
    [101] Minor renaming.
    
    [101] Update for Dart 2.
    
    [101] Missing dependency.
    
    [101] Dart 2.
    
    [101] Dart 2.
    
    [101] Dart 2.
    
    [101] Dart 2.
    
    [101] Correcting model.
    
    [Meta] README file.
    
    [Meta] Clarification.
    
    [101] Copy correction.
    
    [104] Updating data.
    
    [101] Correcting widget class. (#23)
    
    Add link to actual codelab in the README
    
    [Meta] README correction.
    
    [101] README specificity.
    
    Add the slanted_menu.png to assets for 101 branch so its ready when needed. (#44)
    
    [101] TODOs for starter code (#50)
    
    [101] Added future TODOs to 101 starter
    
    [101] Included lines to ignore .iml files and files in idea/ (#67)
    
    [101] Remove 104 comments
    
    [All] Icons for Android and iOS. (#69)
    
    [101] Add .vscode/ to gitignore, stop tracking ignored files (#71)
    
    Removes files that should not be tracked because they've been ignored by gitignore rules (but have yet to be removed from the repository itself). Also adds the .vscode folder to gitignore, for users who might use VSCode as their development environment.
    
    [101] Removed legacy 'new' and 'const' from 101 starter codelab (#72)
    
    [101] Restore legacy to data.dart and product.dart (#75)
    
    [101] Update import for app.dart to stop type errors in future codelabs (#83)
    
    Replace data.dart (#99)
    
    [All] Adding back in filter functionality. (#112)
    
    [106] Update TODO from "PrimaryColorOverride" to "AccentColorOverride" (#115)
    
    * [106] Update TODO from "PrimaryColorOverride" to "AccentColorOverride"
    
    * [106] Update TODO from "PrimaryColorOverride" to "AccentColorOverride"

---
+++ mdc_100_series/lib/login.dart
@@ -0,0 +1,52 @@
+// Copyright 2018-present the Flutter authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  // TODO: Add text editing controllers (101)
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          children: <Widget>[
+            SizedBox(height: 80.0),
+            Column(
+              children: <Widget>[
+                Image.asset('assets/diamond.png'),
+                SizedBox(height: 16.0),
+                Text('SHRINE'),
+              ],
+            ),
+            SizedBox(height: 120.0),
+            // TODO: Wrap Username with AccentColorOverride (103)
+            // TODO: Remove filled: true values (103)
+            // TODO: Wrap Password with AccentColorOverride (103)
+            // TODO: Add TextField widgets (101)
+            // TODO: Add button bar (101)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// TODO: Add AccentColorOverride (103)
