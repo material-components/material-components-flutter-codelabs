87b2d5af1eb64fc6611f7aef74e2b9b5a4e63195
Will Larche <larche@google.com>
2018-04-27 12:17:42 -0400

    [101] Making AppBar something you add in 102.

--- mdc_100_series/lib/home.dart
+++ mdc_100_series/lib/home.dart
@@ -18,9 +18,6 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: const Text('SHRINE'),
-      ),
       body: const Center(
         child: const Text('You did it!'),
       ),
