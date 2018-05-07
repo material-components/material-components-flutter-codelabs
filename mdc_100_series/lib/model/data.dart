849eac96f9ec1736b6d4b7440ddf49f0678e4612
Mary Xia <maryxia@google.com>
2018-05-06 17:09:59 -0700

    add backdrop and working menu with filtering

--- mdc_100_series/lib/model/data.dart
+++ mdc_100_series/lib/model/data.dart
@@ -1,21 +1,7 @@
-// Copyright 2018-present the Flutter authors. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 import 'product.dart';
 
-List<Product> getAllProducts() {
-  return const <Product>[
+List<Product> getProducts(Category category) {
+  const allProducts = <Product>[
     Product(
       category: Category.accessories,
       id: 0,
@@ -283,4 +269,11 @@ List<Product> getAllProducts() {
       price: 58,
     ),
   ];
+  if (category == Category.all) {
+    return allProducts;
+  } else {
+    return allProducts.where((Product p) {
+      return p.category == category;
+    }).toList();
+  }
 }
