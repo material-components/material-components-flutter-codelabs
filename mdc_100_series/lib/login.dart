ccf7a0cb0ae6f0f4799f529192c68189137b008d
Will Larche <larche@google.com>
2018-05-07 01:57:08 -0700

    [101] Update for Dart 2

--- mdc_100_series/lib/login.dart
+++ mdc_100_series/lib/login.dart
@@ -16,56 +16,56 @@ import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
   @override
-  _LoginPageState createState() => new _LoginPageState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final _usernameController = new TextEditingController();
-  final _passwordController = new TextEditingController();
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      body: new SafeArea(
-        child: new ListView(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
           children: <Widget>[
-            const SizedBox(height: 80.0),
-            new Column(
+            SizedBox(height: 80.0),
+            Column(
               children: <Widget>[
-                new Image.asset('assets/diamond.png'),
-                const SizedBox(height: 16.0),
-                const Text('SHRINE'),
+                Image.asset('assets/diamond.png'),
+                SizedBox(height: 16.0),
+                Text('SHRINE'),
               ],
             ),
-            const SizedBox(height: 120.0),
-            new TextField(
+            SizedBox(height: 120.0),
+            TextField(
               controller: _usernameController,
-              decoration: const InputDecoration(
+              decoration: InputDecoration(
                 filled: true,
                 labelText: 'Username',
               ),
             ),
-            const SizedBox(height: 12.0),
-            new TextField(
+            SizedBox(height: 12.0),
+            TextField(
               controller: _passwordController,
-              decoration: const InputDecoration(
+              decoration: InputDecoration(
                 filled: true,
                 labelText: 'Password',
               ),
               obscureText: true,
             ),
-            new ButtonBar(
+            ButtonBar(
               children: <Widget>[
-                new FlatButton(
-                  child: const Text('CANCEL'),
+                FlatButton(
+                  child: Text('CANCEL'),
                   onPressed: () {
                     _usernameController.clear();
                     _passwordController.clear();
                   },
                 ),
-                new RaisedButton(
-                  child: const Text('NEXT'),
+                RaisedButton(
+                  child: Text('NEXT'),
                   onPressed: () {
                     Navigator.pop(context);
                   },
