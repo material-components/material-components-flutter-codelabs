871d4455a050d115dcfe0e719b9c4b9e53fa0275
Mary Xia <maryxia@google.com>
2018-05-06 17:09:59 -0700

    add backdrop and working menu with filtering

--- mdc_100_series/lib/home.dart
+++ mdc_100_series/lib/home.dart
@@ -19,34 +19,12 @@ import 'model/product.dart';
 import 'supplemental/asymmetric_view.dart';
 
 class HomePage extends StatelessWidget {
+  final Category category;
+
+  const HomePage({this.category: Category.all});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        brightness: Brightness.light,
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          onPressed: () {
-            print('Menu button');
-          },
-        ),
-        title: Text('SHRINE'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {
-              print('Search button');
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.tune),
-            onPressed: () {
-              print('Filter button');
-            },
-          ),
-        ],
-      ),
-      body: AsymmetricView(products: getProducts(Category.all))),
-    );
+    return AsymmetricView(products: getProducts(category));
   }
 }
