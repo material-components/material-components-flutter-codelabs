b7a6ebe683a0e7668a93b268004947fdde4841d8
Will Larche <larche@google.com>
2018-05-11 16:20:53 -0700

    [103] Color const correction.

--- mdc_100_series/lib/colors.dart
+++ mdc_100_series/lib/colors.dart
@@ -23,4 +23,4 @@ const kShrineBrown900 = const Color(0xFF442B2D);
 const kShrineErrorRed = const Color(0xFFC5032B);
 
 const kShrineSurfaceWhite = const Color(0xFFFFFBFA);
-const kShrineBackgroundWhite = const Color(0xFFFFFFFF);
+const kShrineBackgroundWhite = Colors.white;
