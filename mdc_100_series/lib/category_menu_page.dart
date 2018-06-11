13b45410b9722c448ea0303c27c5cafb103b25ec
Will Larche <larche@google.com>
2018-06-11 14:33:31 -0400

    [104] PR feedback.

--- mdc_100_series/lib/category_menu_page.dart
+++ mdc_100_series/lib/category_menu_page.dart
@@ -26,41 +26,43 @@ class CategoryMenuPage extends StatelessWidget {
   const CategoryMenuPage({
     Key key,
     @required this.currentCategory,
-    this.onCategoryTap,
-  }) : assert(currentCategory != null);
+    @required this.onCategoryTap,
+  })  : assert(currentCategory != null),
+        assert(onCategoryTap != null);
 
   Widget _buildCategory(Category category, BuildContext context) {
-    var categoryString =
+    final categoryString =
         category.toString().replaceAll('Category.', '').toUpperCase();
+    final ThemeData theme = Theme.of(context);
     return GestureDetector(
       onTap: () => onCategoryTap(category),
       child: category == currentCategory
-          ? Column(
-              children: <Widget>[
-                SizedBox(height: 16.0),
-                Text(
-                  categoryString,
-                  style: Theme.of(context).textTheme.body2,
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 14.0),
-                Container(
-                  width: 70.0,
-                  height: 2.0,
-                  color: Color(0xFFEAA4A4),
-                ),
-              ],
-            )
-          : Container(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Text(
-                categoryString,
-                style: Theme.of(context).textTheme.body2.copyWith(
-                      color: kShrineBrown900.withAlpha(153),
-                    ),
-                textAlign: TextAlign.center,
-              ),
+        ? Column(
+          children: <Widget>[
+            SizedBox(height: 16.0),
+            Text(
+              categoryString,
+              style: theme.textTheme.body2,
+              textAlign: TextAlign.center,
             ),
+            SizedBox(height: 14.0),
+            Container(
+              width: 70.0,
+              height: 2.0,
+              color: Color(0xFFEAA4A4),
+            ),
+          ],
+        )
+      : Container(
+        padding: EdgeInsets.symmetric(vertical: 16.0),
+        child: Text(
+          categoryString,
+          style: theme.textTheme.body2.copyWith(
+              color: kShrineBrown900.withAlpha(153)
+            ),
+          textAlign: TextAlign.center,
+        ),
+      ),
     );
   }
 
