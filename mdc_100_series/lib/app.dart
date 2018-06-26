a4a2ccb2af55182fef624c8d3bd785d7aa791f24
Michelle Dudley <mcdudley@google.com>
2018-06-26 17:13:50 -0400

    Revert "Remove import Todos and changing ordering of a Todo in app.dart."
    This reverts commit 93ed0ba7f86f13389f175978ca47cddbd5b1848f.
    
    Accidentally committed mdc_100_series.iml, reverting to remove this commit.

--- mdc_100_series/lib/app.dart
+++ mdc_100_series/lib/app.dart
@@ -13,13 +13,14 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
-
+// TODO: Import backdrop.dart (104)
 import 'colors.dart';
 import 'home.dart';
 import 'login.dart';
+// TODO: Import CategoryMenuPage (104)
 import 'supplemental/cut_corners_border.dart';
 
-// TODO: Convert ShrineApp to stateful widget (104)
+// TODO: Convert ShrineApp to stateful widget(104)
 class ShrineApp extends StatelessWidget {
   // TODO: Add variable for selected Category (104)
   // TODO: Add a callback when a Category is tapped (104)
@@ -30,9 +31,9 @@ class ShrineApp extends StatelessWidget {
       title: 'Shrine',
       // TODO: Change home: to a Backdrop with a HomePage frontLayer (104)
       home: HomePage(),
-      // TODO: Make currentCategory field take _currentCategory (104)
       // TODO: Pass _currentCategory for frontLayer (104)
       // TODO: Change backLayer field value to CategoryMenuPage (104)
+      // TODO: Make currentCategory field take _currentCategory
       initialRoute: '/login',
       onGenerateRoute: _getRoute,
       theme: _kShrineTheme,
