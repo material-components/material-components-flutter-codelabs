f33e4831d88355c498be4d19dcd4eaec9b3719ec
jpe90 <patriot.game@gmail.com>
2020-07-06 19:32:28 -0400

    Changed variable name to adhere to Google's documented best practices: https://dart.dev/guides/language/effective-dart/style#dont-use-prefix-letters

--- mdc_100_series/lib/colors.dart
+++ mdc_100_series/lib/colors.dart
@@ -14,14 +14,14 @@
 
 import 'package:flutter/material.dart';
 
-const kShrinePink50 = const Color(0xFFFEEAE6);
-const kShrinePink100 = const Color(0xFFFEDBD0);
-const kShrinePink300 = const Color(0xFFFBB8AC);
-const kShrinePink400 = const Color(0xFFEAA4A4);
+const shrinePink50 = const Color(0xFFFEEAE6);
+const shrinePink100 = const Color(0xFFFEDBD0);
+const shrinePink300 = const Color(0xFFFBB8AC);
+const shrinePink400 = const Color(0xFFEAA4A4);
 
-const kShrineBrown900 = const Color(0xFF442B2D);
+const shrineBrown900 = const Color(0xFF442B2D);
 
-const kShrineErrorRed = const Color(0xFFC5032B);
+const shrineErrorRed = const Color(0xFFC5032B);
 
-const kShrineSurfaceWhite = const Color(0xFFFFFBFA);
-const kShrineBackgroundWhite = Colors.white;
+const shrineSurfaceWhite = const Color(0xFFFFFBFA);
+const shrineBackgroundWhite = Colors.white;
