f2ed3e276328da740ce552e6f3e2be4bf63dff9c
Tianguang Zhang <penn.zht@gmail.com>
2020-11-24 19:20:41 +0100

    Migrate.

--- mdc_100_series/lib/model/product.dart
+++ mdc_100_series/lib/model/product.dart
@@ -12,22 +12,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import 'package:flutter/foundation.dart';
-
 enum Category { all, accessories, clothing, home, }
 
 class Product {
   const Product({
-    @required this.category,
-    @required this.id,
-    @required this.isFeatured,
-    @required this.name,
-    @required this.price,
-  })  : assert(category != null),
-        assert(id != null),
-        assert(isFeatured != null),
-        assert(name != null),
-        assert(price != null);
+    required this.category,
+    required this.id,
+    required this.isFeatured,
+    required this.name,
+    required this.price,
+  });
 
   final Category category;
   final int id;
